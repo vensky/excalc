04cf69742fa0904eb1896d3482bdb9a9a77bd86e
vensky <ivan.fedotovskikh@gmail.com>
2021-07-16 11:34:21 +0500

    Создает проект vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 
-Vue.config.productionTip = false
+import 'vuetify/dist/vuetify.min.css';
+
+/*import App from './App.vue';*/
+
+Vue.use(Vuetify)
+/*Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')*/

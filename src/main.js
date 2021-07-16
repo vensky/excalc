9679d5cf7cf68116647f8419da53d0b69a4b1e2b
vensky <ivan.fedotovskikh@gmail.com>
2021-07-16 15:09:57 +0500

    Добавляет верстку

--- src/main.js
+++ src/main.js
@@ -3,11 +3,4 @@ import Vuetify from 'vuetify';
 
 import 'vuetify/dist/vuetify.min.css';
 
-/*import App from './App.vue';*/
-
-Vue.use(Vuetify)
-/*Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')*/
+Vue.use(Vuetify);

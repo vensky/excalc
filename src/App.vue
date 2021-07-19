6575d6864ba8ad0fc927e8806c3464ccbf9b724c
vensky <ivan.fedotovskikh@gmail.com>
2021-07-19 20:49:48 +0500

    Добавляет компонент ExCalc

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,15 @@
 <template>
     <div id="app">
-         <v-app id="excalc">
-                <v-main>
-                    <v-container class="fill-height" fluid>
-                        <v-row align="center" justify="center">
-                            <v-col cols="12" sm="8" md="4">
-                                <v-card class="elevation-12 pb-6">
-                                    <v-toolbar color="grey lighten-3" dark flat>
-                                        <v-toolbar-title class="toolbar__title">Калькулятор конвертации валюты</v-toolbar-title>
-                                        <v-spacer></v-spacer>
-                                    </v-toolbar>
-                              <!--       <v-card-text>
-                                        <v-form
-                                            id="form"
-                                            @submit="onSubmit"
-                                        >
-                                            <v-text-field
-                                                label="Сумма, руб"
-                                                placeholder=0
-                                                type="text"
-                                                inputmode="numeric"
-                                                pattern="[0-9]*"
-                                                :rules="[rules.amount]"
-                                                v-model="form.amount"
-                                                @input="calculateAmountCurrency"
-                                            >
-                                            </v-text-field>
-                                            <v-select
-                                                label="Валюта для конвертации"
-                                                :items="currencies"
-                                                item-text="currency"
-                                                return-object
-                                                v-model="form.currency"
-                                                @change="calculateAmountCurrency"
-                                            >
-                                            </v-select>
-                                            <v-text-field
-                                                label="Сумма в валюте"
-                                                placeholder=0
-                                                type="text"
-                                                readonly
-                                                v-model="form.amountCurrency"
-
-                                            >
-                                            </v-text-field>
-                                            <v-text-field
-                                                label="E-mail"
-                                                type="text"
-                                                v-model="form.email"
-                                                :rules="[rules.required, rules.email]"
-                                            >
-                                            </v-text-field>
-                                        </v-form>
-                                    </v-card-text> -->
-                                    <v-card-actions class="justify-center">
-                                        <v-btn
-                                            color="primary"
-                                            type="submit"
-                                            form="form"
-                                        >
-                                            Записаться на обмен валюты
-                                        </v-btn>
-                                    </v-card-actions>
-                                </v-card>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                </v-main>
-            </v-app>
+        <ExCalc />
     </div>
 </template>
 
 <script>
+    import ExCalc from "@/components/ExCalc.vue"
     export default {
         name: 'app',
-        components: {}
+        components: {
+            ExCalc
+        }
     }
 </script>
-
-<style>
-    #excalc {
-        background: linear-gradient(#1976d2, #fff);
-    }
-
-    .toolbar__title {
-        color: #1976d2;
-    }
-</style>
---
+++ src/components/ExCalc.vue
@@ -0,0 +1,144 @@
+<template>
+    <v-app id="excalc">
+        <v-main>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8" md="4">
+                        <v-card class="elevation-12 pb-6">
+                            <v-toolbar color="grey lighten-3" dark flat>
+                                <v-toolbar-title class="toolbar__title">Калькулятор конвертации валюты</v-toolbar-title>
+                                <v-spacer></v-spacer>
+                            </v-toolbar>
+                             <v-card-text>
+                                <v-form
+                                    id="form"
+                                    @submit="onSubmit"
+                                >
+                                    <v-text-field
+                                        label="Сумма, руб"
+                                        placeholder=0
+                                        type="text"
+                                        inputmode="numeric"
+                                        pattern="[0-9]*"
+                                        :rules="[rules.amount]"
+                                        v-model="form.amount"
+                                        @input="calculateAmountCurrency"
+                                    >
+                                    </v-text-field>
+                                    <v-select
+                                        label="Валюта для конвертации"
+                                        :items="currencies"
+                                        item-text="currency"
+                                        return-object
+                                        v-model="form.currency"
+                                        @change="calculateAmountCurrency"
+                                    >
+                                    </v-select>
+                                    <v-text-field
+                                        label="Сумма в валюте"
+                                        placeholder=0
+                                        type="text"
+                                        readonly
+                                        v-model="form.amountCurrency"
+
+                                    >
+                                    </v-text-field>
+                                    <v-text-field
+                                        label="E-mail"
+                                        type="text"
+                                        v-model="form.email"
+                                        :rules="[rules.required, rules.email]"
+                                    >
+                                    </v-text-field>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions class="justify-center">
+                                <v-btn
+                                    color="primary"
+                                    type="submit"
+                                    form="form"
+                                >
+                                    Записаться на обмен валюты
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'ExCalc',
+    data: () => ({
+        form: {
+            amount: null,
+            currency: null,
+            amountCurrency: null,
+            email: null,
+        },
+        currencies: [
+            {
+                currency: 'Доллар США',
+                rate: 0.013479911
+            },
+            {
+                currency: 'Евро',
+                rate: 0.011421705
+            },
+            {
+                currency: 'Фунт стерлингов Соединенного королевства',
+                rate: 0.009768183
+            },
+            {
+                currency: 'Японская иена',
+                rate: 1.4849124
+            },
+        ],
+
+        /* Описываем правила для валидации полей*/
+        rules: {
+            amount: value => {
+                const pattern = /^[\d.,]*$/
+                return pattern.test(value) || 'Введите число'
+            },
+            required: value => !!value || 'Укажите e-mail',
+            email: value => {
+                const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                return pattern.test(value) || 'Некорректный e-mail'
+            },
+        }
+    }),
+    methods: {
+        /* Описываем функцию для формулу расчета курса*/
+        calculateAmountCurrency() {
+            /* Преобразуем введеную сумму из строки в число*/
+            const amount = parseFloat(this.form.amount);
+            /* Преобразуем выбранный курс валюты из строки в число, если валюта выбрана*/
+            const rate = this.form.currency && parseFloat(this.form.currency.rate);
+            /* Если и сумма, и курс это числа, то выводим произведение, иначе ничего не выводим*/
+            this.form.amountCurrency = (amount && rate) ? (amount * rate).toFixed(2) : ''
+            return (amount && rate) ? (amount * rate).toFixed(2) : '';
+        },
+        onSubmit(e) {
+            e.preventDefault();
+            axios
+              .get('https://www.cbr-xml-daily.ru/daily_json.js')
+              .then(response => (console.log(response)));
+        }
+    }
+}
+</script>
+
+<style>
+#excalc {
+    background: linear-gradient(#1976d2, #fff);
+}
+
+.toolbar__title {
+    color: #1976d2;
+}
+</style>
